cd7cd16da300f8ac9f51d4eee9997d9c51a8bc09
HADEERsAMER <dudusamir172@gmail.com>
2024-08-10 16:20:51 -0700

    lastcommit

--- src/Components/Infowrapper/Infowrapper.jsx
+++ src/Components/Infowrapper/Infowrapper.jsx
@@ -14,7 +14,7 @@ function Infowrapper(){
     <label className='coming'>SOON</label>
     <p id='paragraph'>Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
     </div>
-    <div>
+    <div id='inputdiv'>
    <input id='inputfield' type='Email Address' required />
    <button id='minibtn'>
     <img id='arrow' src={arrow}/>
